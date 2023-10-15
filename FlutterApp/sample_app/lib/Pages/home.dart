8a4b4dfe2b0d7fbf388d3aff1ed75a45ec4f2b72
AndyHsu <andyhsu@zhtech.com.tw>
2023-10-15 21:24:28 +0800

    Flutter Add Login Page & Add DB bak

---
+++ FlutterApp/sample_app/lib/Pages/home.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import 'package:sample_app/BaseConfig/config_globalvar.dart';
+import 'package:sample_app/BaseConfig/config_materialapp.dart';
+
+import 'package:sample_app/Layout/cls_appbar.dart';
+import 'package:sample_app/Layout/cls_bottombar.dart';
+
+import 'package:sample_app/Layout/widget_body.dart';
+import 'package:sample_app/Layout/widget_destination.dart';
+import 'package:sample_app/Layout/widget_drawer.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          colorScheme: ThemeDataConfig1.colorScheme,
+          useMaterial3: ThemeDataConfig1.useMaterial3),
+      home: const MyHomePage(),
+      navigatorKey: GlobalVar.gNavState,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// 當前頁面索引值
+  int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  /// 頁面切換事件
+  void onPageChange(int index) {
+    setState(() {
+      debugPrint(index.toString());
+      currentPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void topBarLeadingOnPressed() {
+      GlobalVar.gScafKey.currentState!.openDrawer();
+      // Scaffold.of(context).openDrawer();
+    }
+
+    void topBarOnPressed_1() {
+      debugPrint("topBarOnPressed_1");
+    }
+
+    void topBarOnPressed_2() {
+      debugPrint("topBarOnPressed_2");
+    }
+
+    var topBar = ClsAppBar1(context, "Flutter Demo", topBarLeadingOnPressed,
+        topBarOnPressed_1, topBarOnPressed_2);
+
+    var bottomBar = ClsBottomBar1(
+        currentPageIndex: currentPageIndex,
+        onPageChange: onPageChange,
+        indicatorColor: Colors.amber[800],
+        destinations: pageDestination());
+
+    var pBody = pagebody(context, currentPageIndex);
+
+    var drawer = drawer1("AndyHsu", "andyhsu@zhtech.com.tw");
+
+    return Scaffold(
+        key: GlobalVar.gScafKey,
+        drawer: drawer,
+        appBar: topBar,
+        bottomNavigationBar: bottomBar,
+        body: pBody);
+  }
+}
--- FlutterApp/sample_app/lib/main.dart
+++ FlutterApp/sample_app/lib/main.dart
@@ -1,93 +1,33 @@
 import 'package:flutter/material.dart';
-
-import 'package:sample_app/BaseConfig/config_materialapp.dart';
-import 'package:sample_app/BaseConfig/config_globalvar.dart';
-
-import 'package:sample_app/Layout/cls_appbar.dart';
-import 'package:sample_app/Layout/cls_bottombar.dart';
-
-import 'package:sample_app/Layout/widget_body.dart';
-import 'package:sample_app/Layout/widget_destination.dart';
-import 'package:sample_app/Layout/widget_drawer.dart';
+import 'package:sample_app/Pages/login.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(const Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Main extends StatefulWidget {
+  const Main({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: ThemeDataConfig1.colorScheme,
-          useMaterial3: ThemeDataConfig1.useMaterial3),
-      home: const MyHomePage(),
-      navigatorKey: GlobalVar.gNavState,
-    );
-  }
+  State<Main> createState() => _MainState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  /// 當前頁面索引值
-  int currentPageIndex = 0;
-
+class _MainState extends State<Main> {
   @override
   void initState() {
     super.initState();
   }
 
-  /// 頁面切換事件
-  void onPageChange(int index) {
-    setState(() {
-      debugPrint(index.toString());
-      currentPageIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    void topBarLeadingOnPressed() {
-      GlobalVar.gScafKey.currentState!.openDrawer();
-      // Scaffold.of(context).openDrawer();
-    }
-
-    void topBarOnPressed_1() {
-      debugPrint("topBarOnPressed_1");
-    }
-
-    void topBarOnPressed_2() {
-      debugPrint("topBarOnPressed_2");
-    }
-
-    var topBar = ClsAppBar1(context, "Flutter Demo", topBarLeadingOnPressed,
-        topBarOnPressed_1, topBarOnPressed_2);
-
-    var bottomBar = ClsBottomBar1(
-        currentPageIndex: currentPageIndex,
-        onPageChange: onPageChange,
-        indicatorColor: Colors.amber[800],
-        destinations: pageDestination());
-
-    var pBody = pagebody(context, currentPageIndex);
-
-    var drawer = drawer1("AndyHsu", "andyhsu@zhtech.com.tw");
-
-    return Scaffold(
-        key: GlobalVar.gScafKey,
-        drawer: drawer,
-        appBar: topBar,
-        bottomNavigationBar: bottomBar,
-        body: pBody);
+    return MaterialApp(
+      theme: ThemeData.from(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(32, 63, 129, 1.0),
+        ),
+      ),
+      home: const Login(),
+    );
   }
 }
